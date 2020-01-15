bd06f903c013513c4d27683b15e36e5f8073cce7
aaalhabib <aaalhabib@outlook.com>
2020-01-15 18:35:18 +0000

    build vue+fix db ctreation

--- users-ms/run.sh
+++ users-ms/run.sh
@@ -1,2 +1,3 @@
 npm install --only=production
-npx pm2 start /opt/flix-users/src/index.js --no-daemon --name my_flix-users --watch /opt/flix-users --ignore-watch node_modules
+#add 20 sec for db to create DBs & users
+sleep 20 && npx pm2 start /opt/flix-users/src/index.js --no-daemon --name my_flix-users --watch /opt/flix-users --ignore-watch node_modules
