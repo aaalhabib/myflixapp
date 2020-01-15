60125e845df0178bec6d21f931b564fc7e4188d6
aaalhabib <aaalhabib@outlook.com>
2020-01-15 20:12:36 +0000

    fix 1.0

--- ui-ms/vue.config.js
+++ ui-ms/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
+
+  // devServer Options don't belong into `configureWebpack`
+  devServer: {
+    host: "0.0.0.0",
+    hot: true,
+    disableHostCheck: true
+  }
 };
