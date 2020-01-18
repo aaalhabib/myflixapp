597525fb7c3f9cb6f071dbb18c5c46862ce8aa31
aaalhabib <aaalhabib@outlook.com>
2020-01-18 14:03:22 +0000

    fix 1.8

--- users-ms/run.sh
+++ users-ms/run.sh
@@ -1,4 +1,3 @@
 npm install --only=production
 #add 20 sec for db to create DBs & users
-#sleep 100 && npx pm2 start /opt/flix-users/src/index.js --no-daemon --name my_flix-users --watch /opt/flix-users --ignore-watch node_modules
-sleep 20 && npm run dev
+sleep 20 && npx pm2 start /opt/flix-users/src/index.js --no-daemon --name my_flix-users --watch /opt/flix-users --ignore-watch node_modules
