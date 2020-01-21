5423bb7fb1f2b0ade6165c7a1c3fa6ed0f07f36b
aaalhabib <aaalhabib@outlook.com>
2020-01-21 13:23:17 +0000

    Generate payment ms app

--- users-ms/src/models/users.model.js
+++ users-ms/src/models/users.model.js
@@ -36,20 +36,20 @@ module.exports = function (app) {
   db.schema.hasTable('users').then(exists => {
     if (!exists) {
       db.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-      .createTable("users", table => {
-        table.uuid("id").primary().notNullable().defaultTo(db.raw('uuid_generate_v4()'))
-        table.text("name");
-        table.text("email").unique();
-        table.text("password").notNullable();
-        table.text("security_question");
-        table.text("security_answer");
+        .createTable('users', table => {
+          table.uuid('id').primary().notNullable().defaultTo(db.raw('uuid_generate_v4()'));
+          table.text('name');
+          table.text('email').unique();
+          table.text('password').notNullable();
+          table.text('security_question');
+          table.text('security_answer');
   
-        table.timestamp("created_at").defaultTo(db.raw('now()'));
-        table.timestamp("updated_at").defaultTo(db.raw('now()'));
+          table.timestamp('created_at').defaultTo(db.raw('now()'));
+          table.timestamp('updated_at').defaultTo(db.raw('now()'));
   
-      })
-      .then(()=>console.log('users created'))
-      .catch(err => console.log(err)) 
+        })
+        .then(()=>console.log('users created'))
+        .catch(err => console.log(err)); 
     }
   })
     .catch(e => console.error('Error creating users table', e)); // eslint-disable-line no-console
