3d239df5e6d06cb5d9b35300f6823037cb5ab02c
aaalhabib <aaalhabib@outlook.com>
2020-01-23 19:42:51 +0000

    fix production startup

--- users-ms/run.production.sh
+++ users-ms/run.production.sh
@@ -1,3 +1,3 @@
 npm install --only=production
 #add 20 sec for db to create DBs & users
-sleep 20 && npx pm2 start /opt/flix-users/src/index.js --name my_flix-users
+sleep 20 && npx pm2 start /opt/flix-users/src/index.js --name my_flix-users --no-daemon
