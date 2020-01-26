a4fa164b0e2db7bbb431898fe90c6fa4a863d300
aaalhabib <aaalhabib@outlook.com>
2020-01-26 23:37:44 +0000

    permision fex on deployment dir for ngnix

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [[ $APP_ENV == 'production' ]]; then
 export volume_path=/var/lib/docker/volumes/jenkins_home/_data/workspace/myflixapp
 docker-compose --no-ansi down 
-docker-compose --no-ansi -f docker-compose.yml -f production.yml up --no-color -d #run both files on prod for the nginx volume to be built
+docker-compose --no-ansi  up --no-color -d #run both files on prod for the nginx volume to be built -f docker-compose.yml -f production.yml
 docker-compose  logs --no-color 
 else
 export volume_path=./
