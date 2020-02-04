a62bfb7a414909e7f10623088cea2b198e94d2b6
aaalhabib <aaalhabib@outlook.com>
2020-02-04 19:16:25 +0000

    add user unique constrain

--- recommendations-ms/run.production.sh
+++ recommendations-ms/run.production.sh
@@ -1,3 +1,3 @@
 npm install --only=production 
 #add 20 sec for db to create DBs 
-npx pm2 start /opt/flix-recommendations/src/index.js --name my_flix-recommendations --no-daemon
+sleep 10 && npx pm2 start /opt/flix-recommendations/src/index.js --name my_flix-recommendations --no-daemon
