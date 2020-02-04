7ff580f7606cb5e1d85e8e06bdef8c8ce33faef5
aaalhabib <aaalhabib@outlook.com>
2020-02-04 19:10:49 +0000

    add user unique constrain

--- recommendations-ms/src/neo4j.js
+++ recommendations-ms/src/neo4j.js
@@ -1,7 +1,7 @@
 const neo4j = require("neo4j-driver");
 
 module.exports = async function(app) {
-  const driver = neo4j.driver(
+  const driver = await neo4j.driver(
     process.env.GDB_CON,
     neo4j.auth.basic(process.env.GDB_USER, process.env.GDB_PASS)
   );
