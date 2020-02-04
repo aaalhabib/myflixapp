bd38a66fc34fb7ed7ba5f38c4722ce935e589cfd
aaalhabib <aaalhabib@outlook.com>
2020-02-04 17:54:02 +0000

    console log the nginx logs

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $APP_ENV == 'production' ]]; then
 export volume_path=/var/lib/docker/volumes/jenkins_home/_data/workspace/myflixapp
-rm -rf graph-data
+sudo rm -rf graph-data
 docker-compose --no-ansi down 
 docker-compose --no-ansi  up --no-color -d #run both files on prod for the nginx volume to be built -f docker-compose.yml -f production.yml
 docker-compose  logs --no-color 
