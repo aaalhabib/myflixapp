457ef60da1bb4fe03e23d928bf167fd9968b4bb9
aaalhabib <aaalhabib@outlook.com>
2020-02-06 16:16:44 +0000

    send build

--- videos-ms/run.production.sh
+++ videos-ms/run.production.sh
@@ -1,6 +1,6 @@
 npm install --only=production 
-#sleep 10 
-#npx knex migrate:latest
-#npx knex seed:run
+sleep 10 
+npx knex migrate:latest
+npx knex seed:run
 #add 20 sec for db to create DBs 
 npx pm2 start /opt/flix-videos/src/index.js --name my_flix-videos --no-daemon
--- watches-ms/run.production.sh
+++ watches-ms/run.production.sh
@@ -1,4 +1,4 @@
 npm install --only=production 
-#sleep 10 && npx knex migrate:latest
+sleep 10 && npx knex migrate:latest
 #add 20 sec for db to create DBs 
 npx pm2 start /opt/flix-watches/src/index.js --name my_flix-watches --no-daemon
