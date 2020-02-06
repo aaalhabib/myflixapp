df29a2c946588edc1de838b825a6482ab0ed2cf5
aaalhabib <aaalhabib@outlook.com>
2020-02-06 14:48:10 +0000

    rebuilding in a new jenkins

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $APP_ENV == 'production' ]]; then
-export volume_path=/var/lib/docker/volumes/jenkins_home/_data/workspace/myflixapp
+export volume_path=/var/lib/docker/volumes/jenkins_home/_data/workspace/myflix
 docker-compose --no-ansi down 
 docker-compose --no-ansi  up --build --no-color -d #run both files on prod for the nginx volume to be built -f docker-compose.yml -f production.yml
 docker-compose  logs --no-color 
